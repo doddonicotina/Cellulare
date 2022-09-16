996a6d0524fe49920245227bb55f829b443681e8
doddonicotina <74264293+doddonicotina@users.noreply.github.com>
2022-09-16 13:57:34 +0200

    Add files via upload

--- Cell.cpp
+++ Cell.cpp
@@ -23,3 +23,63 @@ Cell::Cell(string P, int B, int m, int n):Produttore(P),Batteria(B),minchiamate(
 {
     cout<<"Parameter constructor \n";
 }
+// get 
+int::Cell getBatteria()
+{
+    return Batteria;
+}
+
+string::Cell getProduttore()
+{
+return Produttore;
+}
+
+void::Cell chiamata(int durata)
+{
+    if (durata%2==0)
+    {
+    Batteria = getBatteria() - durata/2;
+}else{
+    durata++;
+    Batteria = getBatteria() - durata/2;
+
+}
+if (batteria>1){
+
+}else{
+    batteria = 0;
+}
+}
+
+string::Cell getSerialN()
+{
+    return Serial_Number;
+}
+
+int::Cell getMchia()
+{
+    return minchiamate;
+}
+
+int::Cell getNsms()
+{
+    return nsms;
+}
+//set
+
+void::Cell setBatteria(int b)
+{
+    Batteria=b;
+}
+void::Cell setMchia(int m)
+{
+    minchiamate=m;
+}
+void::Cell setNsms(int n)
+{
+    nsms=n;
+}
+void::Cell setProduttore(string p)
+{
+    Produttore=p;
+}
